d78fd331a821f31d0bff29b0e18b26f362fa950b
vishaal-0402 <136201906+vishaal-0402@users.noreply.github.com>
2025-11-15 23:27:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-demo-for-githubactions"
+    bucket         = "terraform-demo-for-githubaction"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
