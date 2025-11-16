b24089ce1587faac0d0ee37313aa4bc3e643c7e6
vishaal-0402 <136201906+vishaal-0402@users.noreply.github.com>
2025-11-16 06:25:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# ---------------------------
+# VPC
+# ---------------------------
+resource "aws_vpc" "main" {
+  cidr_block           = "10.20.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "simple-vpc"
+  }
+}
+
+# ---------------------------
+# Output
+# ---------------------------
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
