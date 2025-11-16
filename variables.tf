099bf4047f84db78951cd144c31db1934eb07e1c
vishaal-0402 <136201906+vishaal-0402@users.noreply.github.com>
2025-11-16 06:27:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,1 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
 
-variable "project" {
-  description = "Project name used for resource naming"
-  type        = string
-  default     = "demo-terraform-github"
-}
